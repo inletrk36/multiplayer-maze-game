50b2e827116602437a0fe1211620c4e5d4394428
inletrk36 <invictusrunyan@gmail.com>
2026-05-30 16:09:16 -0600

    Update server.js

--- multiplayer-maze-game/server.js
+++ multiplayer-maze-game/server.js
@@ -11,31 +11,120 @@ const io = new Server(server, {
     cors: { origin: "*" }
 });
 
-let players = {};
+const rooms = {};
+const players = {};
+
+function makeCode() {
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let code = "";
+
+    for (let i = 0; i < 4; i++) {
+        code += chars[Math.floor(Math.random() * chars.length)];
+    }
+
+    return code;
+}
 
 io.on("connection", (socket) => {
+
     console.log("connected:", socket.id);
 
-    players[socket.id] = {
-        x: 0,
-        y: 0,
-        color: `hsl(${Math.random()*360},100%,50%)`
-    };
+    socket.on("createRoom", () => {
 
-    io.emit("state", players);
+        let code;
+
+        do {
+            code = makeCode();
+        } while (rooms[code]);
+
+        rooms[code] = {
+            players: {}
+        };
+
+        socket.join(code);
+
+        players[socket.id] = {
+            room: code,
+            x: 0,
+            y: 0,
+            color: `hsl(${Math.random()*360},100%,50%)`
+        };
+
+        rooms[code].players[socket.id] = players[socket.id];
+
+        socket.emit("roomCreated", code);
+
+        io.to(code).emit("state", rooms[code].players);
+
+        console.log("Room created:", code);
+    });
+
+    socket.on("joinRoom", (code) => {
+
+        code = String(code).toUpperCase();
+
+        if (!rooms[code]) {
+            socket.emit("errorMessage", "Room not found");
+            return;
+        }
+
+        socket.join(code);
+
+        players[socket.id] = {
+            room: code,
+            x: 0,
+            y: 0,
+            color: `hsl(${Math.random()*360},100%,50%)`
+        };
+
+        rooms[code].players[socket.id] = players[socket.id];
+
+        socket.emit("joinedRoom", code);
+
+        io.to(code).emit("state", rooms[code].players);
+
+        console.log(socket.id, "joined", code);
+    });
 
     socket.on("move", (data) => {
-        if (!players[socket.id]) return;
 
-        players[socket.id].x = data.x;
-        players[socket.id].y = data.y;
+        const p = players[socket.id];
 
-        io.emit("state", players);
+        if (!p) return;
+
+        p.x = data.x;
+        p.y = data.y;
+
+        io.to(p.room).emit(
+            "state",
+            rooms[p.room].players
+        );
     });
 
     socket.on("disconnect", () => {
+
+        const p = players[socket.id];
+
+        if (!p) return;
+
+        const room = p.room;
+
+        delete rooms[room].players[socket.id];
         delete players[socket.id];
-        io.emit("state", players);
+
+        io.to(room).emit(
+            "state",
+            rooms[room].players
+        );
+
+        if (
+            Object.keys(
+                rooms[room].players
+            ).length === 0
+        ) {
+            delete rooms[room];
+            console.log("Deleted room:", room);
+        }
     });
 });
 
